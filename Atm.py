5da100ee8f6d33ce19af0111860b948a5d3da652
vikash <114284706+vikash789584@users.noreply.github.com>
2023-06-16 13:35:31 +0530

    Create Atm.py

---
+++ Atm.py
@@ -0,0 +1,11 @@
+pin=123
+chance=3
+while chance>0:
+    userpin=int(input("welcome to SBI ATM, please enter ur pin"))
+    if pin==userpin:
+        print("Welcome, what opertion u want to perform")
+        break
+    else:
+        print("wrong pin, please try again")
+
+    chance-=1
